cf12a05ec4aff0e25e05e41e5f1c313bd77c359c
Coo van Leeuwen <info@cerem.co>
2019-01-09 15:05:04 +0700

    it's sorta working now, tho meta data isn't updated

--- server.ts
+++ server.ts
@@ -24,6 +24,8 @@ const DIST_FOLDER = join(process.cwd(), 'dist');
 // Our index.html we'll use as our template
 const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+
 app.engine('html', (_, options, callback) => {
   renderModuleFactory(AppServerModuleNgFactory, {
     // Our index.html
