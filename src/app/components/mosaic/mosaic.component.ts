51f7a2254d24c936859aa4949258beefa5d78406
Coo van Leeuwen <info@cerem.co>
2019-02-10 22:28:09 +0800

    mosaic nonsense

--- src/app/components/mosaic/mosaic.component.ts
+++ src/app/components/mosaic/mosaic.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ItemService } from 'src/services/item-service';
+
 import { MosaicItem } from '../../model/ApiResult';
-import { container } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-mosaic',
   templateUrl: './mosaic.component.html',
   styleUrls: ['./mosaic.component.scss']
 })
-export class MosaicComponent implements OnInit, AfterViewChecked {
+export class MosaicComponent implements OnInit, AfterViewInit {
   @ViewChild('container') container: ElementRef;
 
   squareSize: number = 50;
-  @Input() cols = 6;
-  @Input() rows = 3;
+  itemsToLoad = 32;
+  wantedSize = 150;
+  showLines = 2;
 
   squares: MosaicItem[] = [];
 
@@ -22,22 +23,27 @@ export class MosaicComponent implements OnInit, AfterViewChecked {
   }
 
   private async load() {
-    this.squares = await this.itemService.getMosaic(this.cols * this.rows);
+    this.squares = await this.itemService.getMosaic(this.itemsToLoad);
   }
 
   public ngOnInit() {
   }
 
-  public ngAfterViewChecked() {
+  public ngAfterViewInit() {
+    console.log("ngAfterViewChecked");
     this.calcSquareSize();
   }
 
   public onResize() {
+    //todo debounce
     this.calcSquareSize();
   }
 
   private calcSquareSize() {
-    this.squareSize = this.container.nativeElement.getBoundingClientRect().width / this.cols;
+    console.log("Recalc");
+    const containerWidth = this.container.nativeElement.getBoundingClientRect().width;
+    const imagesPerRow = Math.floor(containerWidth / this.wantedSize);
+    this.squareSize = Math.floor(containerWidth / imagesPerRow);
   }
 
 }
