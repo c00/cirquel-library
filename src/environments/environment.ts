82531a4e288934bc174893f2656db62c0cbdc695
Coo van Leeuwen <info@cerem.co>
2019-02-11 14:23:58 +0800

    more stuff!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@ import { EnvironmentBase } from './environment.base';
 //const ip = "192.168.1.46";
 const ip = "127.0.0.1";
 
-/* export const environment: EnvironmentBase = {
+export const environment: EnvironmentBase = {
   production: false,
   api: `http://${ip}/cirque/cirque-api/src/public/`,
   itemImgRoot: `http://${ip}/cirque/user-images/`, 
   environment: 'Test'
-}; */
+};
 
-export const environment: EnvironmentBase = {
+/* export const environment: EnvironmentBase = {
   production: false,
   api: 'https://api.cirquelapp.com/',
   itemImgRoot: 'https://images.cirquelapp.com/',
   environment: 'Test'
-};
+}; */
